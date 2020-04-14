4f5a074336c5c9eced9224f65927c24cc5023e61
Aharonla <31625734+Aharonla@users.noreply.github.com>
2020-04-14 18:00:47 +0300

    Create setters.js
    basic exercise code

---
+++ setters.js
@@ -0,0 +1,13 @@
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  
+};
